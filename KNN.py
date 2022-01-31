5d8b61ed24fac7570e8f9972b2ec5d91802eacd6
Sumukh <sumukhjadhav007@gmail.com>
2022-01-31 12:14:15 +0530

    Updated KNN according to manual

--- KNN.py
+++ KNN.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 import numpy as np
 
 dataset=load_iris()
@@ -10,9 +11,13 @@ X_train,X_test,y_train,y_test=train_test_split(dataset["data"],dataset["target"]
 kn=KNeighborsClassifier(n_neighbors=1)
 kn.fit(X_train,y_train)
 
+print("y_pred   y_test")
+
 for i in range(len(X_test)):
     x=X_test[i]
     x_new=np.array([x])
     prediction=kn.predict(x_new)
-    print("TARGET=",y_test[i],dataset["target_names"][y_test[i]],"PREDICTED=",prediction,dataset["target_names"][prediction])
-print(kn.score(X_test,y_test))
+    print(y_test[i],"       ",int(prediction))
+
+
+print("Accuracy of our model is equal:",kn.score(X_test,y_test)*100)
