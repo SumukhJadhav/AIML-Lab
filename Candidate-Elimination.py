c69c44fca9ca3548049f5d5bdd83e7a40c70215f
Sumukh <sumukhjadhav007@gmail.com>
2022-03-23 22:37:34 +0530

    Shorter version for candidate-elimination

--- Candidate-Elimination.py
+++ Candidate-Elimination.py
@@ -1,71 +1,34 @@
-import numpy as np
-import pandas as pd
+import csv
+with open("trainingdata.csv") as f:
+    csv_file = csv.reader(f)
+    data = list(csv_file)
+    print(data)
 
-# Loading Data from a CSV File
-data = pd.read_csv('trainingdata.csv')
-print(data)
+    s = data[1][:-1]
+    g = [['?' for i in range(len(s))] for j in range(len(s))]
 
-# Separating concept features from Target
-concepts = np.array(data)[:,0:-1]
-print(concepts)
+    for i in data:
+        if i[-1] == "Yes":
+            for j in range(len(s)):
+                if i[j] != s[j]:
+                    s[j] = '?'
+                    g[j][j] = '?'
 
-# Isolating target into a separate DataFrame
-# copying last column to target array
-target = np.array(data)[:,-1]
-print(target)
-
-def learn(concepts, target):
-    
-    '''
-    learn() function implements the learning method of the Candidate elimination algorithm.
-    Arguments:
-        concepts - a data frame with all the features
-        target - a data frame with corresponding output values
-    '''
-
-    # Initialise S0 with the first instance from concepts
-    # .copy() makes sure a new list is created instead of just pointing to the same memory location
-    specific_h = concepts[0].copy()
-    print("\nInitialization of specific_h and general_h")
-    print(specific_h)
-    #h=["#" for i in range(0,5)]
-    #print(h)
-
-    general_h = [["?" for i in range(len(specific_h))] for i in range(len(specific_h))]
-    print(general_h)
-    # The learning iterations
-    for i, h in enumerate(concepts):
-
-        # Checking if the hypothesis has a positive target
-        if target[i] == "Yes":
-            for x in range(len(specific_h)):
-
-                # Change values in S & G only if values change
-                if h[x] != specific_h[x]:
-                    specific_h[x] = '?'
-                    general_h[x][x] = '?'
-
-        # Checking if the hypothesis has a positive target
-        if target[i] == "No":
-            for x in range(len(specific_h)):
-                # For negative hyposthesis change values only  in G
-                if h[x] != specific_h[x]:
-                    general_h[x][x] = specific_h[x]
+        elif i[-1] == "No":
+            for j in range(len(s)):
+                if i[j] != s[j]:
+                    g[j][j] = s[j]
                 else:
-                    general_h[x][x] = '?'
+                    g[j][j] = "?"
+        print("\nSteps of Candidate Elimination Algorithm", data.index(i) + 1)
+        print(s)
+        print(g)
+    gh = []
+    for i in g:
+        for j in i:
+            if j != '?':
+                gh.append(i)
+                break
+    print("\nFinal specific hypothesis:\n", s)
 
-        print("\nSteps of Candidate Elimination Algorithm",i+1)
-        print(specific_h)
-        print(general_h)
-    
-    # find indices where we have empty rows, meaning those that are unchanged
-    indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]
-    for i in indices:
-        # remove those rows from general_h
-        general_h.remove(['?', '?', '?', '?', '?', '?'])
-    # Return final values
-    return specific_h, general_h
-
-s_final, g_final = learn(concepts, target)
-print("\nFinal Specific_h:", s_final, sep="\n")
-print("\nFinal General_h:", g_final, sep="\n")
+    print("\nFinal general hypothesis:\n", gh)
