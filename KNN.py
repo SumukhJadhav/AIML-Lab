d93170163f36f1733405c45c68556a943c347d87
Sumukh <sumukhjadhav007@gmail.com>
2022-03-24 11:39:27 +0530

    Added Confusion Matrix to KNN

--- KNN.py
+++ KNN.py
@@ -1,7 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 import numpy as np
 
 dataset=load_iris()
@@ -12,12 +11,17 @@ kn=KNeighborsClassifier(n_neighbors=1)
 kn.fit(X_train,y_train)
 
 print("y_pred   y_test")
-
+y_pred = []
 for i in range(len(X_test)):
     x=X_test[i]
     x_new=np.array([x])
     prediction=kn.predict(x_new)
     print(y_test[i],"       ",int(prediction))
+    y_pred.append(int(prediction))
 
 
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(cm)
 print("Accuracy of our model is equal:",kn.score(X_test,y_test)*100)
